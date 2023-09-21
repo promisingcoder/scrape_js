8851f015db2711890d17be9cad80dc3d82456850
يوسف جمال يوسف محمد ناجي <yossefyoko825@gmail.com>
2023-09-21 16:03:54 -0400

    Update api.js

--- api.js
+++ api.js
@@ -74,8 +74,13 @@ app.post('/', express.json(), async (req, res) => {
         }); */
       const images1 = await page.$$eval('ul[data-test-id="feed-images-content"] img', imgs => imgs.map(img => img.src));
 const images2 = await page.$$eval('.feed-images-content img', imgs => imgs.map(img => img.src));
+      if(images1 == images2){
+        results.images = [...images1]
+      } else{
+        result.images = [...images1, ...images2];
+      }
 //images2 = arraysAreEqual(images1, images2) ? [] : images2;
-result.images = [...images1, ...images2];
+
 
         const reactionsElement = await page.$('span[data-test-id="social-actions__reaction-count"]');
         const reactions = reactionsElement ? await page.evaluate(el => parseInt(el.textContent.trim(), 10), reactionsElement) : 'Element not found';
